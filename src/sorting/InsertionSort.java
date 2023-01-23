5ba226bc5eeadb2ba5d049dfd16df5af7e9c034f
Dharmendra Kumar <ddharmendr@gmail.com>
2023-01-23 11:04:09 +0530

    optimization: 1. earlier swapping the number, now just incrementing their position forward 2. avoiding in-place computation every time.

--- src/sorting/InsertionSort.java
+++ src/sorting/InsertionSort.java
@@ -8,34 +8,19 @@ public class InsertionSort {
 
         int[] inputNumbers = {10, 23, 7, 98,9, 2, 1, 30, 45 };
         int length = inputNumbers.length;
-//        int[] arrayWithFixedSIze = new int[5];
-//        arrayWithFixedSIze[2] = 98;
+
 
         for(int index = 1 ; index < length ; index++){
-          int tmp = 0;
+          int key = inputNumbers[index];
+          int prevIndex = index - 1;
 
-          // while loop iteration 1:  the following while look is swapping all the elements
 
 
-          while( index > 0 && inputNumbers[index - 1] > inputNumbers[index]){
-              tmp = inputNumbers[index];
-              inputNumbers[index] = inputNumbers[index  - 1 ];
-              inputNumbers[index  - 1 ] = tmp;
-              index = index-1;
+          while( prevIndex > 0 && inputNumbers[prevIndex] > key){
+              inputNumbers[prevIndex + 1] = inputNumbers[prevIndex]; // shift the numbers to the right
+              prevIndex = prevIndex-1;
           }
-
-            // while loop iteration 2:  the following while look is swapping all the elements
-
-//            int key  = inputNumbers[index];
-//            int j = index - 1 ;
-//
-//            while( j > 0 && inputNumbers[j - 1] > key){
-//                inputNumbers[j+1] = inputNumbers[j];
-//                j = j-1;
-//            }
-//            inputNumbers[j+1] = key;
-
-
+            inputNumbers[prevIndex + 1] = key;
         }
 
         Arrays.stream(inputNumbers).forEach(it -> System.out.println(it));
