77476d652b3eb57064dd6cb21276d04a5ddc285f
Dharmendra Kumar <ddharmendr@gmail.com>
2023-01-24 01:37:37 +0530

    fibonacci using recursion, other ways to do the same. 5 other implementation to do in future.

--- src/recursion/FibonacciSeries.java
+++ src/recursion/FibonacciSeries.java
@@ -1,27 +1,26 @@
 package recursion;
 
-import java.sql.SQLOutput;
+
+/*
+Multiple ways to implement fib series : TODO
+1)  (Use recursion) : Time Complexity: Exponential   : Done
+2)  (Use Dynamic Programming): O(n) for given n
+3)  (Using power of the matrix {{1, 1}, {1, 0}}) : O(n)
+4)  (Using power of the matrix {{1, 1}, {1, 0}}) : O(n) or O(logn)
+5) (Another approach(Using Binet’s formula)  : O(logn), this is because calculating phi^n takes logn time
+ */
+
 
 public class FibonacciSeries {
-    static int n1 = 0, n2 = 1, n3 = 0, sum = 1;
-    static void printFeb(int count){
-        if(count>0){
-            n3 = n1+n2;
-            n1 = n2;
-            n2 = n3;
-            System.out.print(" "+n3);
-            sum = sum+n3;
-//            System.out.print("{sum = " +sum+"}");
-            printFeb(count -1);
-        }
-    }
 
     public static void main(String[] args) {
-        System.out.println("===Let me print your fibonacci===");
-        System.out.print(n1+" "+n2);
-        int count = 10;
-        printFeb(count-2);
-        System.out.println(" and with SUm : "+sum);
+        int n = 9;
+        System.out.println(printFib(n));
+    }
 
+    private static int printFib(int n) {
+        if( n == 0) return 0;
+        else if( n == 1) return 1;
+        else return printFib(n-1) + printFib(n-2);
     }
 }
