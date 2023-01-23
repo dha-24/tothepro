6f89558f3fbd3b367f7f613bb596cfc606d4c798
Dharmendra Kumar <ddharmendr@gmail.com>
2023-01-24 01:21:23 +0530

    updated factorial code, simple and easy

--- src/recursion/Factorial.java
+++ src/recursion/Factorial.java
@@ -2,14 +2,16 @@ package recursion;
 
 public class Factorial {
     public static long calcFact(int n){
-        if(n>1){
-            System.out.println(n);
-            return calcFact(n-1)*n; // or we can use n*calcFact(n-1)
-        }else
-            return 1;
+//        if(n>1){
+//            System.out.println(n);
+//            return calcFact(n-1)*n; // or we can use n*calcFact(n-1)
+//        }else
+//            return 1;
+    if(n == 1) return 1;
+    else return n * calcFact(n-1);
     }
 
     public static void main(String[] args) {
-        System.out.println("Factorial is :" + calcFact(5));
+        System.out.println("Factorial is :" + calcFact(3));
     }
 }
